656e5842d91fdd4e773317c11e912b0619e6734f
BlackPearlBP <brunnopedro.oliveira@gmail.com>
2024-01-19 09:20:24 -0300

    - Feitos ajustes estéticos; - Conserto de menus; - Criação de menus para retorno;
    Update 19-01-2024

--- Funcoes.py
+++ Funcoes.py
@@ -6,17 +6,22 @@ import Verificacao
 import Limpeza
 
 def cadastrar_ferramenta(tensao):
-        nome = str(input("Digite o nome da ferramenta: "))
-        descricao = str(input("Digite o descricao da ferramenta: "))
-        marca = str(input("Digite o marca da ferramenta: "))
+        nome = str(input("Digite o nome da ferramenta: \n=====================================\n"))
+        Limpeza.limpar_tela()
+        descricao = str(input("Digite o descricao da ferramenta: \n=====================================\n"))
+        Limpeza.limpar_tela()
+        marca = str(input("Digite o marca da ferramenta: \n=====================================\n"))
+        Limpeza.limpar_tela()
         codigo = random.randint(0,1000)
-        preco = Verificacao.verificar_float("Digite o preço da ferramenta: ")
-        quantidade_minima = Verificacao.verificar_inteiro("Digite a quantidade mínima: ")
+        preco = Verificacao.verificar_float("Digite o preço da ferramenta: \n=====================================\n")
+        Limpeza.limpar_tela()
+        quantidade_minima = Verificacao.verificar_inteiro("Digite a quantidade mínima: \n=====================================\n")
+        Limpeza.limpar_tela()
         quantidade_atual = 0
         
         escolha = 0
         while escolha != "1" or escolha != "2":
-            escolha = input("Selecione o tipo de tensão da ferramemnta:\n[1] 110V\n[2] 220V\n")
+            escolha = input("=== Selecione o tipo de tensão da ferramemnta ===\n[1] 110V\n[2] 220V\n=====================================\n")
             if escolha == "1":
                 tensao = "110V"
                 return Ferramenta110(nome, descricao, preco, marca, codigo,quantidade_minima, quantidade_atual, tensao)
@@ -30,42 +35,48 @@ def cadastrar_ferramenta(tensao):
 def edita_ferramenta(codigo_informado, lista_ferramentas):
     for ferramenta in lista_ferramentas:
         if codigo_informado == Ferramenta.get_codigo(ferramenta):
-            nome = str(input("Digite o nome da ferramenta: "))
-            descricao = str(input("Digite o descricao da ferramenta: "))
-            marca = str(input("Digite o marca da ferramenta: "))
-            preco = Verificacao.verificar_float(("Digite o preço da ferramenta: "))
-            quantidade_minima = Verificacao.verificar_inteiro("Digite a quantidade mínima: ")
+            nome = str(input("Digite o nome da ferramenta: \n=====================================\n"))
+            Limpeza.limpar_tela()
+            descricao = str(input("Digite o descricao da ferramenta: \n=====================================\n"))
+            Limpeza.limpar_tela()
+            marca = str(input("Digite o marca da ferramenta: \n=====================================\n"))
+            Limpeza.limpar_tela()
+            preco = Verificacao.verificar_float("Digite o preço da ferramenta: \n=====================================\n")
+            Limpeza.limpar_tela()
+            quantidade_minima = Verificacao.verificar_inteiro("Digite a quantidade mínima: \n=====================================\n")
+            Limpeza.limpar_tela()
  
             Ferramenta.set_nome(ferramenta, nome)
             Ferramenta.set_descricao(ferramenta, descricao)
             Ferramenta.set_preco(ferramenta, preco)
             Ferramenta.set_marca(ferramenta, marca)
             Ferramenta.set_quantidade_minima(ferramenta, quantidade_minima)
-            return print("Alterado! ")
+            Limpeza.limpar_tela_timer()
+            return print("==========================\n|       Alterado!        |\n==========================")
     return print("\033[31;1mCódigo não encontrado!\033[m")
 
 def adiciona_ferramenta(codigo_informado, lista_ferramentas):
     for ferramenta in lista_ferramentas:
         if codigo_informado == Ferramenta.get_codigo(ferramenta):
-            quantidade_nova = Verificacao.verificar_inteiro("Digite a quantidade a ser adicionada: ")
+            quantidade_nova = Verificacao.verificar_inteiro("Digite a quantidade a ser adicionada: \n========================================\n")
             quantidade_anterior = Ferramenta.get_quantidade_atual(ferramenta)
             quantidade_final = quantidade_anterior + quantidade_nova
             Ferramenta.set_quantidade_atual(ferramenta, quantidade_final) 
  
-            return print("Adicionado! ")
+            return print("==========================\n|       Adicinado!       |\n==========================")
     return print("\033[31;1mCódigo não encontrado!\033[m")
 
 def remove_ferramenta(codigo_informado, lista_ferramentas):
     for ferramenta in lista_ferramentas:
         if codigo_informado == Ferramenta.get_codigo(ferramenta):
-            quantidade_remover = Verificacao.verificar_inteiro("Digite a quantidade a ser removida: ")
+            quantidade_remover = Verificacao.verificar_inteiro("Digite a quantidade a ser removida: \n========================================\n")
             if quantidade_remover <= Ferramenta.get_quantidade_atual(ferramenta):
                 quantidade_anterior = Ferramenta.get_quantidade_atual(ferramenta)
                 quantidade_final = quantidade_anterior - quantidade_remover
                 Ferramenta.set_quantidade_atual(ferramenta, quantidade_final)
-                return print("Removido! ")
+                return print("==========================\n|       Removido!        |\n==========================")
             else:
-                return print("Não pode remover uma quantidade maior que a atual! ")
+                return print("\033[31;1mNão pode remover uma quantidade maior do que tem no estoque!\033[m")
     return print("\033[31;1mCódigo não encontrado!\033[m")
 
 def excluir_ferramenta(codigo_informado,lista_ferramentas):
@@ -73,16 +84,17 @@ def excluir_ferramenta(codigo_informado,lista_ferramentas):
         if(codigo_informado==Ferramenta.get_codigo(ferramenta)):
             if Ferramenta.get_quantidade_atual(ferramenta) == 0:
                 lista_ferramentas.remove(ferramenta)
-                return print("Excluído! ")
+                return print("\033[31;1m==========================\n|       Excluido!        |\n==========================\033[m")
             else:
-                return print("Ainda há quantidade em estoque!")
+                return print("\033[31;1mAinda há quantidade em estoque!\033[m")
     return print("\033[31;1mCódigo não encontrado!\033[m")
 
 def exibir_marca(lista_ferramentas):
+    escolha = 0
     marca_procurada = input("Digite a marca a ser pesquisada: ")
     for ferramenta in lista_ferramentas:
         if Ferramenta.get_marca(ferramenta) == marca_procurada:
-            print("=== Produtos da Marca ===")
+            print("\033[92;1m=== Produtos da Marca ===\033[m")
             print(f"Codigo: {ferramenta.get_codigo()}")
             print(f"Nome: {ferramenta.get_nome()}")
             print(f"Descrição: {ferramenta.get_descricao()}")
@@ -92,16 +104,29 @@ def exibir_marca(lista_ferramentas):
             print(f"Quantidade atual: {ferramenta.get_quantidade_atual()}")
             print(f"Tensão: {ferramenta.get_tensao()}")
             print("==================================")
+    while escolha != "1":
+        escolha = input("Deseja retornar ao menu? [1] Sim | [2] Não\n")
+        if escolha == "1":
+            break
+        elif escolha == "2":
+            continue
+        else:
+            print("\033[31;1mOpção selecionada inexistente!\033[m")
 
 def exibir_tensao(lista_ferramentas):
-    escolha = input("Selecione o tipo de tensão da ferramemnta para procurar:\n[1] 110V\n[2] 220V\n")
-    while escolha != "1" or escolha != "2":
-        if escolha == "1":tensao_procurada = "110V"
-        elif escolha=="2":tensao_procurada = "220V"
-        else:print("\033[31;1mResposta inválida!\033[m")        
-        for ferramenta in lista_ferramentas:
-            if ferramenta.get_tensao() == tensao_procurada:
-                print("=== Produtos da Marca ===")
+    escolha_menu = 0
+    escolha = input("=== Selecione o tipo de tensão da ferramemnta ===\n[1] 110V\n[2] 220V\n=====================================\n")
+    if escolha == "1":
+        tensao_procurada = "110V"
+    elif escolha=="2":
+        tensao_procurada = "220V"
+    else:
+        print("\033[31;1mResposta inválida!\033[m")
+        return Limpeza.limpar_tela_timer()
+                
+    for ferramenta in lista_ferramentas:
+        if ferramenta.get_tensao() == tensao_procurada:
+                print("\033[92;1m=== Produtos da Tensão ",tensao_procurada,"===\033[m")
                 print(f"Codigo: {ferramenta.get_codigo()}")
                 print(f"Nome: {ferramenta.get_nome()}")
                 print(f"Descrição: {ferramenta.get_descricao()}")
@@ -111,19 +136,27 @@ def exibir_tensao(lista_ferramentas):
                 print(f"Quantidade atual: {ferramenta.get_quantidade_atual()}")
                 print(f"Tensão: {ferramenta.get_tensao()}")
                 print("==================================")    
-            elif len(lista_ferramentas) == 0:
-                print("\033[31;1mNão há ferramentas cadastradas com essa tensão!\033[m")
-            elif ferramenta.get_tensao() != tensao_procurada:
-                continue
-            else:
+        elif len(lista_ferramentas) == 0:
+            print("\033[31;1mNão há ferramentas cadastradas com essa tensão!\033[m")
+        elif ferramenta.get_tensao() != tensao_procurada:
+            continue
+        else:
                 print("\033[31;1mOpção selecionada inexistente!\033[m")
-        break 
+    while escolha_menu != "1":
+        escolha_menu = input("Deseja retornar ao menu? [1] Sim | [2] Não\n")
+        if escolha_menu == "1":
+            break
+        elif escolha_menu == "2":
+            continue
+        else:
+            print("\033[31;1mOpção selecionada inexistente!\033[m")
                     
 
 def verifica_necessidade_compra(lista_ferramentas):
+    escolha = 0
     for ferramenta in lista_ferramentas:
-        if Ferramenta.get_quantidade_atual(ferramenta) <= Ferramenta.get_quantidade_minima(ferramenta):
-            print("=== Necessita Repôr Estoque ===")
+        if ferramenta.get_quantidade_atual() <= ferramenta.get_quantidade_minima():
+            print("\033[31;1m=== Necessita repôr estoque! ===\033[m")
             print(f"Codigo: {ferramenta.get_codigo()}")
             print(f"Nome: {ferramenta.get_nome()}")
             print(f"Descrição: {ferramenta.get_descricao()}")
@@ -133,12 +166,33 @@ def verifica_necessidade_compra(lista_ferramentas):
             print(f"Quantidade atual: {ferramenta.get_quantidade_atual()}")
             print(f"Tensão: {ferramenta.get_tensao()}")
             print("==================================")
+        else:
+            print("\033[96;1m= Não há necessidade de compra! =\033[m")
+            print(f"Codigo: {ferramenta.get_codigo()}")
+            print(f"Nome: {ferramenta.get_nome()}")
+            print(f"Descrição: {ferramenta.get_descricao()}")
+            print(f"Marca: {ferramenta.get_marca()}")
+            print(f"Preço: R${ferramenta.get_preco()}")
+            print(f"Quantidade minima: {ferramenta.get_quantidade_minima()}")
+            print(f"Quantidade atual: {ferramenta.get_quantidade_atual()}")
+            print(f"Tensão: {ferramenta.get_tensao()}")
+            print("==================================")
+    while escolha != "1":
+        escolha = input("Deseja retornar ao menu? [1] Sim | [2] Não\n")
+        if escolha == "1":
+            break
+        elif escolha == "2":
+            continue
+        else:
+            print("\033[31;1mOpção selecionada inexistente!\033[m")
+
 
 def exibir_codigo(lista_ferramentas):
-    codigo_procurado = Verificacao.verificar_inteiro(int(input("Digite o código do produto: ")))
+    escolha = 0
+    codigo_procurado = Verificacao.verificar_inteiro("Digite o código do produto: ")
     for ferramenta in lista_ferramentas:
         if Ferramenta.get_codigo(ferramenta) == codigo_procurado:
-            print("=== Informações do Produto ===")
+            print("\033[94m=== Informações do Produto ===\033[m")
             print(f"Codigo: {ferramenta.get_codigo()}")
             print(f"Nome: {ferramenta.get_nome()}")
             print(f"Descrição: {ferramenta.get_descricao()}")
@@ -148,11 +202,20 @@ def exibir_codigo(lista_ferramentas):
             print(f"Quantidade atual: {ferramenta.get_quantidade_atual()}")
             print(f"Tensão: {ferramenta.get_tensao()}")
             print("==================================")
+    while escolha != "1":
+        escolha = input("Deseja retornar ao menu? [1] Sim | [2] Não\n")
+        if escolha == "1":
+            break
+        elif escolha == "2":
+            continue
+        else:
+            print("\033[31;1mOpção selecionada inexistente!\033[m")
 
 def exibir_informacoes(lista_ferramentas):
+    escolha = 0
     Limpeza.limpar_tela()
     for ferramenta in lista_ferramentas:
-        print("=== Informações da Ferramenta ===")
+        print("\033[94m=== Informações do Produto ===\033[m")
         print(f"Codigo: {ferramenta.get_codigo()}")
         print(f"Nome: {ferramenta.get_nome()}")
         print(f"Descrição: {ferramenta.get_descricao()}")
@@ -161,4 +224,12 @@ def exibir_informacoes(lista_ferramentas):
         print(f"Quantidade minima: {ferramenta.get_quantidade_minima()}")
         print(f"Quantidade atual: {ferramenta.get_quantidade_atual()}")
         print(f"Tensão: {ferramenta.get_tensao()}")
-        print("==================================")
+        print("=====================================")
+    while escolha != "1":
+        escolha = input("Deseja retornar ao menu? [1] Sim | [2] Não\n")
+        if escolha == "1":
+            break
+        elif escolha == "2":
+            continue
+        else:
+            print("\033[31;1mOpção selecionada inexistente!\033[m")
--- Principal.py
+++ Principal.py
@@ -3,14 +3,14 @@ import Limpeza
 import Funcoes
 from re import match
 import Verificacao
-import os 
 
 if __name__ == "__main__":
     Limpeza.limpar_tela()
     lista_ferramentas = []
     opcao=0
     while(opcao != 11):
-        opcao = int(input("Selecione uma tela:\n[1] Cadastrar\n[2] Mostrar\n[3] Editar\n[4] Adicionar no Estoque\n[5] Remover do Estoque\n[6] Excluir\n[7] Perquisar por Marca\n[8] Perquisar por Tensão\n[9] Verificar Necessidade de Compra\n[10] Perquisar por Código\n[11] Sair\n"))
+        Limpeza.limpar_tela()
+        opcao = Verificacao.verificar_inteiro("======== Selecione uma Opcão ========\n[1] Cadastrar\n[2] Mostrar\n[3] Editar\n[4] Adicionar no Estoque\n[5] Remover do Estoque\n[6] Excluir\n[7] Pesquisar por Marca\n[8] Pesquisar por Tensão\n[9] Verificar Necessidade de Compra\n[10] Pesquisar por Código\n[11] Sair\n=====================================\n")
         match opcao:
             case 1:
                 #cadastrar ferramenta
@@ -20,7 +20,7 @@ if __name__ == "__main__":
                 Limpeza.limpar_tela()
             case 2:
                 #mostrar ferramenta
-                if len(lista_ferramentas) > 0:
+                if len(lista_ferramentas) != 0:
                         Limpeza.limpar_tela()
                         Funcoes.exibir_informacoes(lista_ferramentas)
                 else:
@@ -28,7 +28,7 @@ if __name__ == "__main__":
                     Limpeza.limpar_tela_timer()
             case 3:
                 #editar ferramenta
-                if len(lista_ferramentas) > 0:
+                if len(lista_ferramentas) != 0:
                     codigo=Verificacao.verificar_inteiro("Informe o código: ")
                     Limpeza.limpar_tela()
                     Funcoes.edita_ferramenta(codigo,lista_ferramentas)
@@ -38,7 +38,7 @@ if __name__ == "__main__":
                 Limpeza.limpar_tela_timer()
             case 4:
                 #adicionar no estoque
-                if len(lista_ferramentas) > 0:
+                if len(lista_ferramentas) != 0:
                     codigo=Verificacao.verificar_inteiro("Informe o código: ")
                     Limpeza.limpar_tela()
                     Funcoes.adiciona_ferramenta(codigo,lista_ferramentas)
@@ -48,7 +48,7 @@ if __name__ == "__main__":
                 Limpeza.limpar_tela_timer()
             case 5:
                 #remover do estoque
-                if len(lista_ferramentas) > 0:
+                if len(lista_ferramentas) != 0:
                     codigo=Verificacao.verificar_inteiro("Informe o código: ")
                     Limpeza.limpar_tela()
                     Funcoes.remove_ferramenta(codigo,lista_ferramentas)
@@ -58,7 +58,7 @@ if __name__ == "__main__":
                 Limpeza.limpar_tela_timer()
             case 6:
                 #excluir xd
-                if len(lista_ferramentas) > 0:
+                if len(lista_ferramentas) != 0:
                     codigo=Verificacao.verificar_inteiro("Informe o código: ")
                     Limpeza.limpar_tela()
                     Funcoes.excluir_ferramenta(codigo,lista_ferramentas)
@@ -68,7 +68,7 @@ if __name__ == "__main__":
                 Limpeza.limpar_tela_timer()
             case 7:
                 #consultar marca
-                if len(lista_ferramentas) > 0:
+                if len(lista_ferramentas) != 0:
                         Limpeza.limpar_tela()
                         Funcoes.exibir_marca(lista_ferramentas)
                 else:
@@ -76,7 +76,7 @@ if __name__ == "__main__":
                     Limpeza.limpar_tela_timer()
             case 8:
                 #consultar tensao
-                if len(lista_ferramentas) > 0:
+                if len(lista_ferramentas) != 0:
                         Limpeza.limpar_tela()
                         Funcoes.exibir_tensao(lista_ferramentas)
                 else:
@@ -84,7 +84,7 @@ if __name__ == "__main__":
                     Limpeza.limpar_tela_timer()
             case 9:
                 #verificar necessidade de compra
-                if len(lista_ferramentas) > 0:
+                if len(lista_ferramentas) != 0:
                         Limpeza.limpar_tela()
                         Funcoes.verifica_necessidade_compra(lista_ferramentas)
                 else:
@@ -92,7 +92,7 @@ if __name__ == "__main__":
                     Limpeza.limpar_tela_timer()
             case 10:
                 #consultar codigo
-                if len(lista_ferramentas) > 0:
+                if len(lista_ferramentas) != 0:
                         Limpeza.limpar_tela()
                         Funcoes.exibir_codigo(lista_ferramentas)
                 else:
